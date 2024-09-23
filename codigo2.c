521f6ea736bcbda1d42e2266feb50f41263f6a67
Luís Campos <109487926+SonOfGod314@users.noreply.github.com>
2024-09-23 17:24:08 -0300

    Create codigo2.c

---
+++ codigo2.c
@@ -0,0 +1,18 @@
+int main(){
+    int z;
+    int x; 
+    int y;
+
+    x = -5;
+    y = 10;
+
+    if (x >= 0){
+        z = 1;
+        printf(z);
+    }    
+    else{
+        z = y;
+        printf(z);
+    }
+    endif
+}
